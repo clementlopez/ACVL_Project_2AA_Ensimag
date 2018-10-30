ee08f84763cadc2322123f3b8b20187158d61e54
clement_lopez (DevLopez) <lopezclement31@gmail.com>
2018-10-30 02:01:08 +0100

    gestion de la multiplicite

--- src/Association.java
+++ src/Association.java
@@ -6,6 +6,8 @@ public class Association {
     Classe extremite;
     boolean estNavigable;
     String typeAssociation;
+    String multipliciteOrigine;
+    String multipliciteExtremite;
 
     public String getNom() {
         return nom;
@@ -15,7 +17,23 @@ public class Association {
         this.nom = nom;
     }
 
-    public String getTypeAssociation() {
+    public String getMultipliciteOrigine() {
+		return multipliciteOrigine;
+	}
+
+	public void setMultipliciteOrigine(String multipliciteOrigine) {
+		this.multipliciteOrigine = multipliciteOrigine;
+	}
+
+	public String getMultipliciteExtremite() {
+		return multipliciteExtremite;
+	}
+
+	public void setMultipliciteExtremite(String multipliciteExtremite) {
+		this.multipliciteExtremite = multipliciteExtremite;
+	}
+
+	public String getTypeAssociation() {
 		return typeAssociation;
 	}
 
@@ -45,6 +63,8 @@ public class Association {
         this.extremite = mExtremite;
         this.estNavigable = true;
         this.typeAssociation = "Association";
+        this.multipliciteOrigine = "*";
+        this.multipliciteExtremite = "*";
     }
 
     public void menu(Scanner sc) {
@@ -67,7 +87,7 @@ public class Association {
 		                break;
 		            case 3:
 		                System.out.println("Vous avez saisi : choix 3");
-		                System.out.println("Non implemente");
+		                modifierMultiplicite(sc);
 		                break;
 		            case 4:
 		                System.out.println("Vous avez saisi : choix 4");
@@ -83,7 +103,49 @@ public class Association {
         }
     }
 
-    void afficherOptionsMenu() {
+    private void modifierMultiplicite(Scanner sc) {
+    	String str;
+    	boolean nouvellemult = false;
+        while (!nouvellemult) {
+        	System.out.println("Quelle multiplicite depuis l'origine ?");
+            str = sc.nextLine();
+            if((!str.equals("0"))&&str.matches("^([0-9]+(,[0-9]+)*)|(([0-9]+\\.\\.)?([0-9]+|\\*))$")) {
+				setMultipliciteExtremite(str);
+				nouvellemult = true;
+			}
+            else {
+            	System.out.println("Il vous faut renseigner une visibilite valide"
+            			+ "\nDes exemples de multiplicites acceptees par ce simulateur de Diagrammes UML sont :"
+            			+ "\n1,12,36,44 ou 2..58 ou 1..* ou encore *"
+            			+ "\n\n Remarque : La multiplicite ne peut pas etre nulle de ce cote de l'association");
+            }
+        }
+        
+        nouvellemult = false;
+        
+        while (!nouvellemult) {
+        	System.out.println("Quelle multiplicite vers l'origine ?");
+            str = sc.nextLine();
+            if(str.matches("^([0-9]+(,[0-9]+)*)|(([0-9]+\\.\\.)?([0-9]+|\\*))$")) {
+				setMultipliciteOrigine(str);
+				nouvellemult = true;
+				if(str.equals("0")) {
+					setEstNavigable(false);
+				}
+				else {
+					setEstNavigable(true);
+				}
+			}
+            else {
+            	System.out.println("Il vous faut renseigner une visibilite valide"
+            			+ "\nDes exemples de multiplicites acceptees par ce simulateur de Diagrammes UML sont :"
+            			+ "\n1,12,36,44 ou 2..58 ou 1..* ou 0 ou encore *");
+            }
+        }
+		
+	}
+
+	void afficherOptionsMenu() {
     	System.out.println("########################");
         System.out.println("** Menu Association **");
         System.out.println("Que voulez vous faire ?");
@@ -114,16 +176,19 @@ public class Association {
         } else {
             System.out.println("\t\tNavigable en sens unique");
         }
-        System.out.println("\t\t\tEntre " + getOrigine().getNom());
-        System.out.println("\t\t\tEt " + getExtremite().getNom());
+        System.out.println("\t\t\t " + getOrigine().getNom() + " "
+        			+ getMultipliciteOrigine() + " ----- " + getMultipliciteExtremite()
+        			+ " " + getExtremite().getNom());
     }
     
     private void changeNavigabilite() {
         setEstNavigable(!isEstNavigable());
         if (isEstNavigable()) {
             System.out.println("L'association " + getNom() + " est desormais navigable en double sens");
+            setMultipliciteOrigine("*");
         } else {
             System.out.println("L'association " + getNom() + " n'est desormais navigable qu'en sens unique");
+            setMultipliciteOrigine("0");
         }
     }
     
